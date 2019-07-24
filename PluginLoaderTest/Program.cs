32be1ab307210bac88f50bf2a6722c185eb4702a
Eric Rozell <erozell@outlook.com>
2019-07-24 08:22:33 -0400

    Initial commit.

---
+++ PluginLoaderTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Composition.Hosting;
+using System.IO;
+using McMaster.NETCore.Plugins;
+using Models;
+
+namespace PluginLoaderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assemblyFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "plugins", "TestPlugin", "TestPlugin.dll");
+            var assembly = PluginLoader.CreateFromAssemblyFile(assemblyFile, PluginLoaderOptions.PreferSharedTypes)
+                .LoadDefaultAssembly();
+            var test = new ContainerConfiguration()
+                .WithAssembly(assembly)
+                .CreateContainer()
+                .GetExport<ITest>();
+            test.Test();
+        }
+    }
+}
---
+++ TestPlugin/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Composition;
+using Microsoft.CognitiveServices.Speech;
+using Models;
+
+namespace TestPlugin
+{
+    [Export(typeof(ITest))]
+    public class Test : ITest
+    {
+        void ITest.Test()
+        {
+            var speechConfig = SpeechConfig.FromSubscription("key", "region");
+        }
+    }
+}
